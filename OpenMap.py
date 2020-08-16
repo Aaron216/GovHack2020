c7c95b51ea3fde470cc9906d84d365e2845466ab
Aaron Musgrave <aaron@winthropdc.com>
2020-08-16 10:30:03 +1000

    Added ACT traffic data to map

--- OpenMap.py
+++ OpenMap.py
@@ -1,5 +1,11 @@
-import folium
+# GovHack 2020
+# Team DISER
+# Open Map: An aggregation of low-quality geo-spatial data points
 
+import folium
+import requests
+
+# Generate Map
 australianCities = {
     'Canberra': [-35.293056, 149.126944],
     'Sydney': [-33.865, 151.209444]
@@ -10,6 +16,7 @@ mapData = folium.Map(
     title = 'Open Map'
 )
 
+# Add sample fire Data
 fireData = folium.FeatureGroup(
     name='Fire'
 )
@@ -19,7 +26,8 @@ fireData.add_child(
         popup = 'Fire',
         icon = folium.Icon(
             icon = 'fire',
-            color = 'red')
+            color = 'red'
+        )
     )
 )
 fireData.add_child(
@@ -33,27 +41,35 @@ fireData.add_child(
     )
 )
 
+# Add ACT traffic data
 trafficData = folium.FeatureGroup(
     name='Trafic'
 )
-trafficData.add_child(
-    folium.Marker(
-        location = [-35.29276, 149.12694],
-        popup = 'Bridge Closed',
-        icon = folium.Icon(
-            icon = 'warning-sign',
-            color = 'orange')
+
+response = requests.get('https://services1.arcgis.com/E5n4f1VY84i0xSjy/arcgis/rest/services/Public_Temporary_Road_Closures/FeatureServer/0/query?&where=objectid%3E0&outFields=*&f=json')
+if response.status_code != 200:
+    print('Error getting ACT Road Closure Data')
+    exit()
+for item in response.json()['features']:
+    description = str(item['attributes']['roadsClosed']).strip()
+    geometryX = item['geometry']['x']
+    geometryY = item['geometry']['y']
+
+    trafficData.add_child(
+        folium.Marker(
+            location = [geometryY, geometryX],
+            popup = folium.Popup(
+                html = description,
+                min_width = 200,
+                max_width = 400
+            ),
+            icon = folium.Icon(
+                icon = 'warning-sign',
+                color = 'orange'
+            )
+        )
     )
-)
-trafficData.add_child(
-    folium.Marker(
-        location = [-35.30170, 149.14110],
-        popup = 'Bridge Closed',
-        icon = folium.Icon(
-            icon = 'warning-sign',
-            color = 'orange')
-    )
-)
+
 
 mapData.add_child(fireData)
 mapData.add_child(trafficData)
